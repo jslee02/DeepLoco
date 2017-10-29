a4b6323d0ca6368f02c05b3d3fcf4873d053fdad
Jeongseok Lee <jslee02@gmail.com>
2017-10-28 22:02:00 -0700

    Enable Travis CI -- just installing dependencies

---
+++ .travis/install.sh
@@ -0,0 +1,28 @@
+APT='
+cmake
+libboost-all-dev
+libeigen3-dev
+freeglut3-dev
+libxi-dev
+libxmu-dev
+libbullet-dev
+'
+
+sudo apt-get -qq --yes --force-yes install $APT
+
+CAFFE_DEPENDS='
+libprotobuf-dev
+libleveldb-dev
+libsnappy-dev
+libopencv-dev
+libhdf5-serial-dev
+protobuf-compiler
+libboost-all-dev
+libatlas-base-dev
+libopenblas-dev
+libgflags-dev
+libgoogle-glog-dev
+liblmdb-dev
+'
+
+sudo apt-get -qq --yes --force-yes install $CAFFE_DEPENDS
---
+++ .travis/script.sh
@@ -0,0 +1,3 @@
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j4
