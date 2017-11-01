8a688b8f4437cbfa7b07c72fe1f704efc85cccfd
Jeongseok Lee <jslee02@gmail.com>
2017-11-01 14:12:43 -0700

    Install Bullet 2.87

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,3 +1,4 @@
+#Install dependencies from apt-get
 APT='
 cmake
 libboost-all-dev
@@ -5,11 +6,10 @@ libeigen3-dev
 freeglut3-dev
 libxi-dev
 libxmu-dev
-libbullet-dev
 '
-
 sudo apt-get -qq --yes --force-yes install $APT
 
+# Install caffe
 CAFFE_DEPENDS='
 doxygen
 python-numpy
@@ -28,10 +28,12 @@ gcc-4.8-multilib g++-4.8-multilib
 libf2c2-dev
 libglew-dev
 '
-
 sudo apt-get -qq --yes --force-yes install $CAFFE_DEPENDS
-
 git clone https://github.com/BVLC/caffe
 cd caffe
-cmake . && make -j4 && make install
+mkdir build && cd build && cmake .. && make -j4 && make install
 
+# Install bullet
+git clone --depth 1 --single-branch --branch 2.87 https://github.com/bulletphysics/bullet3
+cd bullet
+mkdir build && cd build && cmake .. && make -j4 && make install
